664beea19f55e5e850d8818502b07e7e3b5783ce
Moon <mooncactusmoon@gmail.com>
2022-01-08 00:47:59 +0800

    新建modal

--- js/index_js.js
+++ js/index_js.js
@@ -7,4 +7,6 @@ $("tr").click(function(){
     var img=['vote.JPG','calendar.JPG','animal.JPG','band.JPG','basic.JPG','en_ani.JPG','levelc.JPG'];
     // console.log("./img/pj/"+img[index]);
     $("#por_img").attr("src","./img/pj/"+img[index]);
+
+    
 });
