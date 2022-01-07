b4224ec96a0804e7750f3ef445874568fcd3dd67
Moon <mooncactusmoon@gmail.com>
2022-01-08 00:08:52 +0800

    跑馬燈css 增強

--- js/index_js.js
+++ js/index_js.js
@@ -1,5 +1,7 @@
 //點選list切換相對圖片
 $("tr").click(function(){
+    $("tr").removeClass();
+    $(this).addClass("tr_show");
     var index = $("tr").index(this);
     // alert(index);
     var img=['vote.JPG','calendar.JPG','animal.JPG','band.JPG','basic.JPG','en_ani.JPG','levelc.JPG'];
