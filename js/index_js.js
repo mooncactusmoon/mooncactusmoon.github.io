419d10d9d5d9c00362fa00b79d25023655b8dd7c
Moon <mooncactusmoon@gmail.com>
2022-01-08 13:22:53 +0800

    嘗試做modal

--- js/index_js.js
+++ js/index_js.js
@@ -4,9 +4,14 @@ $("tr").click(function(){
     $(this).addClass("tr_show");
     var index = $("tr").index(this);
     // alert(index);
-    var img=['vote.JPG','calendar.JPG','animal.JPG','band.JPG','basic.JPG','en_ani.JPG','levelc.JPG'];
+    var img=['vote','calendar','animal','band','basic','en_ani','levelc'];
     // console.log("./img/pj/"+img[index]);
-    $("#por_img").attr("src","./img/pj/"+img[index]);
+    $("#por_img").attr("src","./img/pj/"+img[index]+".JPG").attr("alt",img[index]);
+
 
     
 });
+
+$("#por_img").click(function(){
+   $("#myModal").find("p").empty().append("HAAAA");
+})
