a5863cfca23562066878d2af9f7f1189193c546d
Moon <mooncactusmoon@gmail.com>
2022-01-11 23:04:31 +0800

    修改好test_index.html的nav bar

--- js/test_js.js
+++ js/test_js.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
     var h=$(window).height();//抓視窗高度
+    var w=$(window).width();//抓視窗高度
+    
+    $(".top").css("height",h+"px"); 
+    $(".top").css("width",w+"px"); 
+
+    
     $(".aside").css("height",h+"px"); 
     // $(".aside").fadeOut();
     $(".aside,.close_b").hide();
 
+    $(".open_b").mouseover(function(){
+        $(this).find("i").removeClass("fa-bars").addClass("fa-arrow-right");
+    })
+    $(".open_b").mouseout(function(){
+        $(this).find("i").removeClass("fa-arrow-right").addClass("fa-bars");
+    })
     $(".open_b").click(function(){
         console.log("AA");
         $(".aside,.close_b").fadeIn();
